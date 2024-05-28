e3df6c4b30b7ddec96d8133070055764e73aa0f4
Laszlo Kosasvari <laszlo_kosasvari@epam.com>
2024-05-28 14:15:39 +0200

    latest

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,36 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@LambdaHandler(lambdaName = "sns_handler",
+	roleName = "sns_handler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(targetTopic = "lambda_topic")
+public class SnsHandler implements RequestHandler<SNSEvent, String> {
+
+	// Configure the logger
+	private static final Logger logger = LogManager.getLogger(SnsHandler.class);
+
+	@Override
+	public String handleRequest(SNSEvent event, Context context) {
+		// Iterate over each record in the SNS event
+		for (SNSEvent.SNSRecord record : event.getRecords()) {
+			// Get the SNS message body
+			String message = record.getSNS().getMessage();
+
+			// Log the message body to CloudWatch
+			logger.info("Received SNS message: {}", message);
+		}
+		return "Successfully processed SNS messages";
+	}
+}
---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,32 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@LambdaHandler(lambdaName = "sqs_handler",
+	roleName = "sqs_handler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 10)
+public class SqsHandler implements RequestHandler<SQSEvent, Void>{
+
+	private static final Logger logger = LogManager.getLogger(SqsHandler.class);
+
+	@Override
+	public Void handleRequest(SQSEvent event, Context context)
+	{
+		for(SQSEvent.SQSMessage msg : event.getRecords()){
+			logger.info("Received SQS message: {}", msg.getBody());
+		}
+		return null;
+	}
+}
+
